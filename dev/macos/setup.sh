a1758ab93487da4f5c8f0c86583d64da1ff90011
Jannis Leidel <jannis@leidel.info>
2021-11-25 11:09:02 +0100

    Sudo make me a sandwich.

--- dev/macos/setup.sh
+++ dev/macos/setup.sh
@@ -7,6 +7,6 @@ echo "Factory config is:"
 grep -e "conda location" -e "conda version" -e "python version" <(conda info -a) | sed 's/^\s*/  /'
 conda install -yq pip conda-build conda-verify pytest
 conda update openssl ca-certificates certifi
-eval "$(sudo /Users/runner/miniconda3/bin/python -m conda init bash --dev)"
+eval "$(python -m conda init bash --dev)"
 echo "Done! Now running:"
 grep -e "conda location" -e "conda version" -e "python version" <(conda info -a) | sed 's/^\s*/  /'
